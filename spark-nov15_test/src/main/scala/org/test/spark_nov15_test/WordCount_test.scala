ef73709a9d1332408fee4fc8cfa438991b2b2e88
deena <deenadayal.muli@gmail.com>
2016-11-15 11:35:21 +0530

    test project for git

---
+++ spark-nov15_test/src/main/scala/org/test/spark_nov15_test/WordCount_test.scala
@@ -0,0 +1,27 @@
+package org.test.spark_nov15_test
+
+
+import org.apache.spark.SparkConf; 
+
+import org.apache.spark.SparkContext;
+
+ 
+
+object WordCount_test {
+  
+  def main(args: Array[String]) ={
+    
+   val conf=new SparkConf()
+     .setAppName("WordCount").setMaster("local");
+   
+   val sc = new SparkContext(conf);
+   
+   val test =sc.textFile("food.txt");
+test.flatMap( line => line.split(" "))
+.map(word=>(word,1))
+.reduceByKey(_+_)
+.saveAsTextFile("FoodCount");
+
+   
+  } 
+}
